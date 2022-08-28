16b9c4289dd7f548ea28c1770de0faee94be0e6f
Wine Ferreira <106812435+Wineferreira@users.noreply.github.com>
2022-08-28 17:19:29 -0300

    poo avançado

---
+++ contador_objetos.py
@@ -0,0 +1,19 @@
+#!python
+from select import select
+
+
+class ClasseSimples:
+    contador = 0
+
+    def __init__(self):
+         # self.__class__.contador += 1
+        self.contar()
+
+    @classmethod
+    def contar(cls):
+        cls.contador +=1
+
+
+if __name__ == '__main__':
+    lista = [ClasseSimples(), ClasseSimples()]
+    print(ClasseSimples.contador)  # Esperado 2
